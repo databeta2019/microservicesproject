af5aa2d1d8ad0b44f7f80d8fa81c5a5a5caf68f7
EC2 Default User <ec2-user@ip-172-31-28-93.us-west-1.compute.internal>
2020-05-25 22:11:31 +0000

    Adding Modifications

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,16 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="2002714/app"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login
+#docker tag local-image:tagname new-repo:tagname
+# REPO has to be created in Docker
+# Tagname is basically a description that you want to give
+docker tag app:latest 2002714/udacityproject:latest
 # Step 3:
 # Push image to a docker repository
+docker push 2002714/udacityproject:latest

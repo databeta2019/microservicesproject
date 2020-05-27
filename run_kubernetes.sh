f639d47b2350e81f75988dd959804a35879c9330
EC2 Default User <ec2-user@ip-172-31-6-158.us-west-1.compute.internal>
2020-05-27 03:29:30 +0000

    Adding Kubernetes Changes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,24 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath="2002714/app"
+dockerpath="udacityproject"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+# kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1
+# kubectl create deployment kubernetes-bootcamp --image=$dockerpath:latest
+docker login
+kubectl create deployment app2 --image=$dockerpath:latest
+# kubectl run flaskskearlndemo\ # RUN YOUR DOCKER IMAGE
+#     # --generator=run-pod/v1\
+#     --image=udacityproject\
+#     --port=80 --labels app=flaskskearlndemo
 # Step 3:
 # List kubernetes pods
-
+# kubectl get deployments
+kubectl get deployments
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+#kubectl port-forward deployment/prediction-as-a-pod 8000:80
+kubectl port-forward app2 8000:80
